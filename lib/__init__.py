efe8652d3debcd4aab8b190a6ba9ad3f7d70ef3f
Nicolas Autexier <nicolas.atx@gmx.fr>
2021-03-26 18:26:15 +0100

    rm init lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,11 @@
 # For Templates pack
-from lib import classifier_trainer
-from lib import data_explore
-from lib import consume_validation
-from lib.models import calibration
-from lib.models import threshold
-from lib.sec import secureLog
-from lib.SqlCo import Sqldd
+# from lib import classifier_trainer
+# from lib import data_explore
+# from lib import consume_validation
+# from lib.models import calibration
+# from lib.models import threshold
+# from lib.sec import secureLog
+# from lib.SqlCo import Sqldd
 
 
 
